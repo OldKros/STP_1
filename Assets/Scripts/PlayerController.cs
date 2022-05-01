28c7cc53891a65b805da908e4a82cfc3a0b94669
Krostik <47256571+OldKros@users.noreply.github.com>
2022-05-01 16:16:57 +0100

    Navmesh movement in 2d

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Tilemaps;
 
 public class PlayerController : MonoBehaviour
@@ -9,6 +10,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed = 5f;
     Camera mainCamera;
     Tilemap tileMap;
+    SpriteRenderer spriteRenderer;
+    NavMeshAgent navMeshAgent;
+    Animator animator;
 
 
     Vector3 destination;
@@ -18,23 +22,49 @@ public class PlayerController : MonoBehaviour
     {
         mainCamera = Camera.main;
         tileMap = GameObject.FindGameObjectWithTag("WalkableTileMap").GetComponent<Tilemap>();
+
     }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdateAnimator();
         if (Input.GetMouseButtonDown(0))
         {
             destination = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            destination.z = transform.position.z;
+            if (destination.x < transform.position.x) spriteRenderer.flipX = true;
+            else spriteRenderer.flipX = false;
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
         }
-        float movementThisFrame = moveSpeed * Time.deltaTime;
-        transform.position = Vector2.MoveTowards(transform.position, destination, movementThisFrame);
+
+        if (navMeshAgent.isStopped) animator.SetBool("isIdle", true);
+        // float movementThisFrame = moveSpeed * Time.deltaTime;
+        // transform.position = Vector2.MoveTowards(transform.position, destination, movementThisFrame);
     }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = navMeshAgent.velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.x + localVelocity.y;
+        Debug.Log(speed);
+        animator.SetFloat("movementSpeed", Mathf.Abs(speed));
+    }
+
+    // private void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     if (other.gameObject.tag == "Static")
+    //     { destination = transform.position; }
+    // }
 }
